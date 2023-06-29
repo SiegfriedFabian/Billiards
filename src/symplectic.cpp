342e16a5356dffe35c2357cad7ee9d79e2878ba1
SiegfriedFabian <landerfabian@gmail.com>
2023-06-29 21:03:25 +0200

    added docking to symplectic billiards

--- src/symplectic.cpp
+++ src/symplectic.cpp
@@ -147,7 +147,16 @@ int main()
 	//imgui stuff
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows // NEEDED FOR MULTI VIEWPORT (DRAGGIND IMGUI WINDOWS OUTSIDE)
 	ImGui::StyleColorsDark();
+	// NEEDED FOR MULTI VIEWPORT (DRAGGIND IMGUI WINDOWS OUTSIDE)
+	// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+	ImGuiStyle& style = ImGui::GetStyle();
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		style.WindowRounding = 0.0f;
+		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+	}
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init("#version 330");
 
@@ -343,6 +352,17 @@ int main()
 			// Renders the ImGUI elements
 			ImGui::Render();
 			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+						// NEEDED FOR MULTI VIEWPORT (DRAGGIND IMGUI WINDOWS OUTSIDE)
+			// Update and Render additional Platform Windows
+			// (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+			//  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
+			if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+			{
+				GLFWwindow* backup_current_context = glfwGetCurrentContext();
+				ImGui::UpdatePlatformWindows();
+				ImGui::RenderPlatformWindowsDefault();
+				glfwMakeContextCurrent(backup_current_context);
+			}
 		}
 
 
